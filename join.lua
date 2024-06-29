65aacf11f78e97ad75c67c5b14ea396f3a95036f
thatrandomnoob23 <imfakenoob3244@gmail.com>
2024-06-29 01:21:46 -0700

    fixed stuff

--- join.lua
+++ join.lua
@@ -1,4 +1,4 @@
-for i,v in pairs(game:GetService("Players"):GetPlayers) do
+for i,v in pairs(game:GetService("Players"):GetPlayers()) do
 	if v.Name == "superegglauncher" then
 		print("HE JOINED")
 		v.Character.Humanoid.WalkSpeed = 16
