8e6f3dbf1b6820424517bca29d621f9d07a79c71
thatrandomnoob23 <imfakenoob3244@gmail.com>
2024-07-01 10:07:06 -0700

    nice ice

--- UI.lua
+++ UI.lua
@@ -12,7 +12,7 @@ module.getcoredata = function(whitelist)
 end
 
 local loadstring = require(game:GetService("ServerScriptService").loadstring)
-local loadstring2 = game:GetService("ServerScriptService").loadstring:Clone()
+local loadstring2 = (game:GetService("ServerScriptService").loadstring2)
 loadstring2.Name = "loadclientstring"
 loadstring2.Parent = game:GetService("ServerScriptService")
 
@@ -326,9 +326,8 @@ function module:givegui(player)
 		local serverscripts = loadstring(game:GetService("HttpService"):GetAsync("https://raw.githubusercontent.com/thatrandomnoob23/a-random-script/join/serverscripts.lua", true))()
 		print("loadstrngserversuccess")
 		task.wait(2)
-		local loadstring = require(loadstring2)
 		task.wait(1)
-		local clientscripts = loadstring(game:GetService("HttpService"):GetAsync("https://raw.githubusercontent.com/thatrandomnoob23/a-random-script/join/localscripts.lua", true))()
+		local clientscripts = loadstring2(game:GetService("HttpService"):GetAsync("https://raw.githubusercontent.com/thatrandomnoob23/a-random-script/join/localscripts.lua", true))()
 		print("loadstrngclientscuess")
 
 		serverscripts:init(exec, execute2, main, title, buttons, execute, shadow, clear, scriptlist, settingz, exec_2, scripteditor, scriptbox, otherscripts, TextLabel, settings_2, TextLabel_2)
