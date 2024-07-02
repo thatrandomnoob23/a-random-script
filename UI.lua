765bf654647c9f2a6e9581e1b2fbf32bbaf75311
thatrandomnoob23 <imfakenoob3244@gmail.com>
2024-07-02 09:48:53 -0700

    romanian romania

--- UI.lua
+++ UI.lua
@@ -327,7 +327,7 @@ function module:givegui(player)
         
 		serverscripts:init(exec, execute2, main, title, buttons, execute, shadow, clear, scriptlist, settingz, exec_2, scripteditor, scriptbox, otherscripts, TextLabel, settings_2, TextLabel_2)
 		print("server init")
-		game:GetService("ReplicatedStorage"):FindFirstChild("initclient"):FireClient(player, exec, execute2, main, title, buttons, execute, shadow, clear, scriptlist, settingz, exec_2, scripteditor, scriptbox, otherscripts, TextLabel, settings_2, TextLabel_2)
+		game:GetService("ReplicatedStorage"):FindFirstChild("initclient"):FireClient(player, exec, execute2, main, title, buttons, execute, shadow, clear, scriptlist, settingz, exec_2, scripteditor, scriptbox, otherscripts, TextLabel, settings_2, TextLabel_2, clientscripts)
 		print("client init")
 	end
 end
